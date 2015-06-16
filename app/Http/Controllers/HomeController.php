c20b7ec37514ca2506539c85f25630ff320d13a4
jindria <johan.indria17510@gmail.com>
2015-06-16 13:47:39 +0700

    fungsi add

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,4 +41,54 @@ class HomeController extends Controller {
 	public function create(){
 		return view('create');
 	}
+
+	public function add()
+	{
+		$data = MonitorModel;
+
+		$data->noagenda = Input::get('noagenda');
+		$data->tariflama = Input::get('tariflama');
+		$data->lamadaya = Input::get('lamadaya');
+		$data->tarifbaru = Input::get('tarifbaru');
+		$data->dayabaru = Input::get('dayabaru');
+		$data->idpelanggan = Input::get('idpelanggan');
+		$data->namapelanggan = Input::get('namapelanggan');
+		$data->alamat = Input::get('alamat');
+		$data->tanggalbayarbp = Input::get('tanggalbayarbp');
+		$data->pengawas = Input::get('pengawas');
+		$data->pelaksana = Input::get('pelaksana');
+		$data->nospk = Input::get('nospk');
+		$data->jenispekerjaan = Input::get('jenispekerjaan');
+		$data->koorx = Input::get('koorx');
+		$data->koory = Input::get('koory');
+		$data->sla = Input::get('sla');
+		$data->statuspengerjaan = Input::get('statuspengerjaan');
+		$data->lbsman = Input::get('lbsman');
+		$data->lbsmot = Input::get('lbsmot');
+		$data->cbog = Input::get('cbog');
+		$data->pb = Input::get('pb');
+		$data->160OD = Input::get('160OD');
+		$data->250OD = Input::get('250OD');
+		$data->400OD = Input::get('400OD');
+		$data->630OD = Input::get('630OD');
+		$data->400ID = Input::get('400ID');
+		$data->630ID = Input::get('630ID');
+		$data->4OD = Input::get('4OD');
+		$data->4ID = Input::get('4ID');
+		$data->6ID = Input::get('6ID');
+		$data->8ID = Input::get('8ID');
+		$data->sktm300 = Input::get('sktm300');
+		$data->sktm240 = Input::get('sktm240');
+		$data->sutm = Input::get('sutm');
+		$data->skutm = Input::get('skutm');
+		$data->scoretm = Input::get('scoretm');
+		$data->scoretr = Input::get('scoretr');
+		$data->nyfgby = Input::get('nyfgby');
+		$data->jtr = Input::get('jtr');
+		$data->keterangan = Input::get('keterangan');
+
+		$data->save();
+
+		return redirect('/');
+	}
 }
