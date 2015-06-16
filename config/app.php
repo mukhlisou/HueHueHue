f9d6d70f7c4e45dc2dbc19d0ea64ba29239495f3
jindria <johan.indria17510@gmail.com>
2015-06-16 18:49:01 +0700

    install excel thrid party

--- config/app.php
+++ config/app.php
@@ -143,6 +143,8 @@ return [
         App\Providers\AppServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        'Maatwebsite\Excel\ExcelServiceProvider',
+
 
     ],
 
@@ -191,6 +193,7 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Excel' => 'Maatwebsite\Excel\Facades\Excel::class',
     ],
 
 ];
