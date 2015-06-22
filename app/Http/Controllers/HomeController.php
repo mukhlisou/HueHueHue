2481d45e69481894b5ac00c05982254980561933
jindria <johan.indria17510@gmail.com>
2015-06-22 11:25:53 +0700

    refined import: handling datatype

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,11 +54,20 @@ class HomeController extends Controller {
 	public function add()
 	{
 
-        $validator = Validator::make(Input::all(), [
-            'noagenda'=>'required',
-            'idpelanggan' => 'required|unique:monitor',
-            'namapelanggan' => 'required',
-        ]);
+        $rules = [
+			'noagenda'=>'required|integer',
+			'idpelanggan' => 'required|integer',
+	        'namapelanggan' => 'required',
+		];
+
+		$messages = [
+			'noagenda.required' => 'No. agenda is required.',
+			'noagenda.integer' => 'No. agenda must be numbers.',
+			'idpelanggan.required' => 'ID pelanggan is required',
+			'idpelanggan.integer' => 'ID pelanggan must be numbers.',
+			'namapelanggan.required' => 'Nama pelanggan is required',
+		];
+        $validator = Validator::make(Input::all(), $rules, $messages);
 
         if ($validator->fails()) {
             return redirect('create')
@@ -119,11 +128,20 @@ class HomeController extends Controller {
     }
 	public function edit($idpelanggan)
 	{
-        $validator = Validator::make(Input::all(), [
-            'noagenda'=>'required',
-            'idpelanggan' => 'required',
-            'namapelanggan' => 'required',
-        ]);
+		$rules = [
+			'noagenda'=>'required|integer',
+			'idpelanggan' => 'required|integer',
+	        'namapelanggan' => 'required',
+		];
+
+		$messages = [
+			'noagenda.required' => 'No. agenda is required.',
+			'noagenda.integer' => 'No. agenda must be numbers.',
+			'idpelanggan.required' => 'ID pelanggan is required',
+			'idpelanggan.integer' => 'ID pelanggan must be numbers.',
+			'namapelanggan.required' => 'Nama pelanggan is required',
+		];
+        $validator = Validator::make(Input::all(), $rules, $messages);
 
         if ($validator->fails()) {
             return redirect()->back()
@@ -191,61 +209,89 @@ class HomeController extends Controller {
 				$reader->noHeading();
 				$reader->toArray();
 				//dd($reader->parsed);
+				$input = array();
+
 				foreach ($reader->parsed as $row) {
 					//dd($row);
 					if(is_numeric($row[0])){
-						$id = MonitorModel::where('idpelanggan',[$row[6]])->first();
 
-						if($id == null){
-							$data = new MonitorModel;
+						$rules = [
+							'1'=>'required|integer',
+							'6' => 'required|integer',
+				            '7' => 'required',
+						];
+
+						$messages = [
+							'1.required' => 'No. agenda is required.',
+							'1.integer' => 'No. agenda must be numbers.',
+							'6.required' => 'ID pelanggan is required',
+							'6.integer' => 'ID pelanggan must be numbers.',
+							'7.required' => 'Nama pelanggan is required',
+						];
+		
+						$validator = Validator::make($row->all(), $rules, $messages);
+
+						if ($validator->fails()) {
+								return redirect()->back()
+								->withErrors($validator)
+								->withInput();
 						}else{
-							$data = MonitorModel::where('idpelanggan',[$row[6]])->first();
+							$id = MonitorModel::where('idpelanggan',[$row[6]])->first();
+							if($id == null){
+								$data = new MonitorModel;
+							}else{
+								$data = MonitorModel::where('idpelanggan',[$row[6]])->first();
+							}
+
+							if($row[1] !=null){$data->noagenda = $row[1];}
+							if($row[2] !=null){$data->tariflama = $row[2];}
+							if($row[3] !=null){$data->lamadaya = $row[3];}
+							if($row[4] !=null){$data->tarifbaru = $row[4];}
+							if($row[5] !=null){$data->dayabaru = $row[5];}
+							if($row[6] !=null){$data->idpelanggan = $row[6];}
+							if($row[7] !=null){$data->namapelanggan = $row[7];}
+							if($row[8] !=null){$data->alamat = $row[8];}
+							if($row[9] !=null){$data->tanggalbayarbp = $row[9];}
+							if($row[10] !=null){$data->pengawas = $row[10];}
+							if($row[11] !=null){$data->pelaksana = $row[11];}
+							if($row[12] !=null){$data->nospk = $row[12];}
+							if($row[13] !=null){$data->jenispekerjaan = $row[13];}
+							if($row[14] !=null){$data->koorx = $row[14];}
+							if($row[15] !=null){$data->koory = $row[15];}
+							if($row[16] !=null){$data->sla = $row[16];}
+							if($row[17] !=null){$data->statuspengerjaan = $row[17];}
+							if($row[18] !=null){$data->lbsman = $row[18];}
+							if($row[19] !=null){$data->lbsmot = $row[19];}
+							if($row[20] !=null){$data->cbog = $row[20];}
+							if($row[21] !=null){$data->pb = $row[21];}
+							if($row[22] !=null){$data->OD160 = $row[22];}
+							if($row[23] !=null){$data->OD250 = $row[23];}
+							if($row[24] !=null){$data->OD400 = $row[24];}
+							if($row[25] !=null){$data->OD630 = $row[25];}
+							if($row[26] !=null){$data->ID400 = $row[26];}
+							if($row[27] !=null){$data->ID630 = $row[27];}
+							if($row[28] !=null){$data->OD4 = $row[28];}
+							if($row[29] !=null){$data->ID4 = $row[29];}
+							if($row[30] !=null){$data->ID6 = $row[30];}
+							if($row[31] !=null){$data->ID8 = $row[31];}
+							if($row[32] !=null){$data->sktm300 = $row[32];}
+							if($row[33] !=null){$data->sktm240 = $row[33];}
+							if($row[34] !=null){$data->sutm = $row[34];}
+							if($row[35] !=null){$data->skutm = $row[35];}
+							if($row[36] !=null){$data->scoretm = $row[36];}
+							if($row[37] !=null){$data->scoretr = $row[37];}
+							if($row[38] !=null){$data->nyfgby = $row[38];}
+							if($row[39] !=null){$data->jtr = $row[39];}
+							if($row[40] !=null){$data->keterangan = $row[40];}
+
+							array_push($input,$data);
 						}
-
-						if($row[1] !=null){$data->noagenda = $row[1];}
-						if($row[2] !=null){$data->tariflama = $row[2];}
-						if($row[3] !=null){$data->lamadaya = $row[3];}
-						if($row[4] !=null){$data->tarifbaru = $row[4];}
-						if($row[5] !=null){$data->dayabaru = $row[5];}
-						if($row[6] !=null){$data->idpelanggan = $row[6];}
-						if($row[7] !=null){$data->namapelanggan = $row[7];}
-						if($row[8] !=null){$data->alamat = $row[8];}
-						if($row[9] !=null){$data->tanggalbayarbp = $row[9];}
-						if($row[10] !=null){$data->pengawas = $row[10];}
-						if($row[11] !=null){$data->pelaksana = $row[11];}
-						if($row[12] !=null){$data->nospk = $row[12];}
-						if($row[13] !=null){$data->jenispekerjaan = $row[13];}
-						if($row[14] !=null){$data->koorx = $row[14];}
-						if($row[15] !=null){$data->koory = $row[15];}
-						if($row[16] !=null){$data->sla = $row[16];}
-						if($row[17] !=null){$data->statuspengerjaan = $row[17];}
-						if($row[18] !=null){$data->lbsman = $row[18];}
-						if($row[19] !=null){$data->lbsmot = $row[19];}
-						if($row[20] !=null){$data->cbog = $row[20];}
-						if($row[21] !=null){$data->pb = $row[21];}
-						if($row[22] !=null){$data->OD160 = $row[22];}
-						if($row[23] !=null){$data->OD250 = $row[23];}
-						if($row[24] !=null){$data->OD400 = $row[24];}
-						if($row[25] !=null){$data->OD630 = $row[25];}
-						if($row[26] !=null){$data->ID400 = $row[26];}
-						if($row[27] !=null){$data->ID630 = $row[27];}
-						if($row[28] !=null){$data->OD4 = $row[28];}
-						if($row[29] !=null){$data->ID4 = $row[29];}
-						if($row[30] !=null){$data->ID6 = $row[30];}
-						if($row[31] !=null){$data->ID8 = $row[31];}
-						if($row[32] !=null){$data->sktm300 = $row[32];}
-						if($row[33] !=null){$data->sktm240 = $row[33];}
-						if($row[34] !=null){$data->sutm = $row[34];}
-						if($row[35] !=null){$data->skutm = $row[35];}
-						if($row[36] !=null){$data->scoretm = $row[36];}
-						if($row[37] !=null){$data->scoretr = $row[37];}
-						if($row[38] !=null){$data->nyfgby = $row[38];}
-						if($row[39] !=null){$data->jtr = $row[39];}
-						if($row[40] !=null){$data->keterangan = $row[40];}
-
-						$data->save();
+						
 					}
 					
+				}
+				foreach($input as $in){
+					$in->save();
 				}
 
 			});
